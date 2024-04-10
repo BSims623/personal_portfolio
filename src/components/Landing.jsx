cbf76443d85cc8c49e5b59ca8bd5fe533229a19b
Brian Sims <briansims623@gmail.com>
2024-04-09 22:11:52 -0700

    fix: margin on quote

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -20,7 +20,7 @@ const Landing = ({ landingRef }) => {
 
     return (
         <section ref={landingRef} id='Landing' className='' style={{ display: 'flex', justifyContent: 'center', alignItems: 'end', width: '100vw', height: '94vh', backgroundImage: `url(${landingImage})`, backgroundSize: 'cover', backgroundPosition: 'center', padding: '0 15px' }}>
-            <div className="mb-3">
+            <div className="mb-4">
                 <div className='d-flex'><i className="align-self-start fa-sharp fa-solid fa-quote-left text-light"></i><p className="quote-text align-self-end text-light my-0 ms-2">{quote}</p></div>
                 <p className="quote-text text-light text-end">- {author}</p>
             </div>
