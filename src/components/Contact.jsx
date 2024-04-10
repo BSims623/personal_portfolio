87a1cf05f9634eef29a31406e8596ea980d4c035
Brian Sims <briansims623@gmail.com>
2024-04-09 18:43:55 -0700

    feat: aria-label contact links

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,9 +5,9 @@ const Contact = ({ contactRef }) => {
         <section ref={contactRef} className='border-top border-warning py-5 d-flex flex-column align-items-center justify-content-center'>
             <h1 className='text-light'>Contact</h1>
             <div className="mt-2  d-flex justify-content-center">
-                <a href="mailto:briansims326@gmail.com"><div className="mx-3 rounded-circle contact-link border border-warning d-flex justify-content-center align-items-center"><i className="btn-icon fa-solid fa-envelope"></i></div></a>
-                <a href="https://github.com/BSims623" target='_blank'><div className="mx-3 rounded-circle contact-link border border-warning d-flex justify-content-center align-items-center"><i className="btn-icon fa-brands fa-github"></i></div></a>
-                <a href="https://www.linkedin.com/in/brian-sims-210747293/" target='_blank'><div className="mx-3 rounded-circle contact-link border border-warning d-flex justify-content-center align-items-center"><i className="btn-icon fa-brands fa-linkedin-in"></i></div></a>
+                <a href="mailto:briansims326@gmail.com" aria-label='Email'><div className="mx-3 rounded-circle contact-link border border-warning d-flex justify-content-center align-items-center"><i className="btn-icon fa-solid fa-envelope"></i></div></a>
+                <a href="https://github.com/BSims623" target='_blank' aria-label='Github'><div className="mx-3 rounded-circle contact-link border border-warning d-flex justify-content-center align-items-center"><i className="btn-icon fa-brands fa-github"></i></div></a>
+                <a href="https://www.linkedin.com/in/brian-sims-210747293/" target='_blank' aria-label='LinkedIn'><div className="mx-3 rounded-circle contact-link border border-warning d-flex justify-content-center align-items-center"><i className="btn-icon fa-brands fa-linkedin-in"></i></div></a>
             </div>
         </section>
     )
